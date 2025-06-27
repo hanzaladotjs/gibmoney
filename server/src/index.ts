ccf7a117a49ecfcc90be6bfff9fa0e9d84971f19
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-06-27 19:17:41 +0530

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { connectDB } from "./utils/db";
 import mainRouter from "./routes/index";
+import cors from "cors"
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/v1", mainRouter);
 

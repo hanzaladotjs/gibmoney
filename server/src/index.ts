30733b96bf3d21c219ab16e2d2b9d0fe96aba60c
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-06-27 20:19:33 +0530

    lol

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,14 @@ import mainRouter from "./routes/index";
 import cors from "cors"
 
 const app = express();
-app.use(express.json());
 app.use(cors({
   origin: "http://localhost:5173", // ✅ Your frontend dev origin
   credentials: true                // ✅ If you're using cookies/auth later
 }));
+
+app.use(express.json());
+
+
 
 
 app.use("/api/v1", mainRouter);
